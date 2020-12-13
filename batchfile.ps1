6f0449c8902b1a5def757070b9fa7184a58f0686
duchessacid <48784313+duchessacid@users.noreply.github.com>
2020-12-13 18:07:57 +0300

    Delete batchfile.ps1

--- batchfile.ps1
+++
@@ -1,5 +0,0 @@
-$temp = $env:temp
-
-wget https://raw.githubusercontent.com/duchessacid/CG_labs/master/batchfile.bat -OutFile $temp\1.bat
-curl https://raw.githubusercontent.com/duchessacid/CG_labs/master/batchfile.bat -OutFile $temp\2.bat
-cmd.exe /c $temp\1.bat
